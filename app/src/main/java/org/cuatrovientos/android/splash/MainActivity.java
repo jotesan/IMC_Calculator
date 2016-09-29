a5e8a542b4670c8d31e299bde87646680c065ae8
jotesan <jotesan357@gmail.com>
2016-09-29 11:34:06 +0200

    Initial commit

---
+++ app/src/main/java/org/cuatrovientos/android/splash/MainActivity.java
@@ -0,0 +1,18 @@
+package org.cuatrovientos.android.splash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openMenu(View view) {
+
+    }
+}
---
+++ app/src/main/java/org/cuatrovientos/android/splash/Menu.java
@@ -0,0 +1,26 @@
+package org.cuatrovientos.android.splash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void openImcCalc(View view) {
+
+    }
+
+    public void openHelp(View view) {
+
+    }
+
+    public void openAbout(View view) {
+
+    }
+}
